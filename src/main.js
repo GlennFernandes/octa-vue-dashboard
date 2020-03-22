69a7a85b812aeab71b7b633de2526805a6c58701
Glenn <glenn@Glenns-MacBook-Pro.local>
2020-03-23 03:00:04 +0530

    Theme and Linting update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from 'vue';
+
+import { MdButton, MdContent, MdCard, MdRipple } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default-dark.css';
+
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdCard);
+Vue.use(MdRipple);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount('#app');
